27cc3c3f08f09c68da84b861d6f88003139b84ab
Luis Rodriguez <luis.rodriguez@globalquakemodel.org>
2015-04-08 10:35:27 +0200

    Mis cambios para graficar

--- hmtk/plotting/mapping_luis.py
+++ hmtk/plotting/mapping_luis.py
@@ -51,7 +51,6 @@
 Module hmtk.plotting.catalogue.map is a graphical
 function for plotting the spatial distribution of events
 '''
-import collections
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
@@ -61,10 +60,9 @@ from openquake.hazardlib.geo.line import Line
 from openquake.hazardlib.geo.polygon import Polygon
 from hmtk.sources.area_source import mtkAreaSource
 from hmtk.sources.point_source import mtkPointSource
+#from obspy.imaging.beachball import Beach
 from hmtk.plotting.beachball import Beach
-from hmtk.plotting.plotting_utils import DISSIMILAR_COLOURLIST
 from hmtk.sources.simple_fault_source import mtkSimpleFaultSource
-from hmtk.sources.complex_fault_source import mtkComplexFaultSource
 
 DEFAULT_SYMBOLOGY = [(-np.inf, 1., 'k.'), # M < 1
                      (1., 2., 'g*'), # 1 < M < 2
@@ -78,9 +76,6 @@ DEFAULT_SYMBOLOGY = [(-np.inf, 1., 'k.'), # M < 1
                      (9., np.inf,'ro')] # 9 < M < 10
 
 LEGEND_OFFSET=(1.3, 1.0)
-PORTRAIT_ASPECT = (6, 8)
-LANDSCAPE_ASPECT = (8, 6)
-NCOLS = len(DISSIMILAR_COLOURLIST)
 
 def _fault_polygon_from_mesh(source):
     """
@@ -100,7 +95,7 @@ class HMTKBaseMap(object):
     Class to plot the spatial distribution of events based in the Catalogue
     imported from hmtk.
     '''
-    def __init__(self, config, title, dpi=300):
+    def __init__(self, config, title, saveplot=False, filetype='png', dpi=1000):
         """
         :param dict config:
             Configuration parameters of the algorithm, containing the
@@ -117,6 +112,8 @@ class HMTKBaseMap(object):
         """
         self.config = config
         self.title = title
+        self.saveplot = saveplot
+        self.filetype = filetype
         self.dpi = dpi
         self.fig = None
         self.m = None
@@ -141,20 +138,19 @@ class HMTKBaseMap(object):
 
         lat0 = lowcrnrlat + ((uppcrnrlat - lowcrnrlat) / 2)
         lon0 = lowcrnrlon + ((uppcrnrlon - lowcrnrlon) / 2)
-        pe = 15
-        gra = 20
+        #pe = 15
+        #gra = 20
         fon = [22]
         if (uppcrnrlat - lowcrnrlat) >= (uppcrnrlon - lowcrnrlon):
-            fig_aspect = (pe, gra)
+            fig_aspect = (15, 20)
         else:
-            fig_aspect = (gra, pe)
-        self.fig = plt.figure(num=None,
+            fig_aspect = (20, 15)
+        self.fig = plt.figure(num=None, 
                               figsize=fig_aspect,
                               dpi=self.dpi,
                               facecolor='w',
                               edgecolor='k')
-        if self.title:
-            plt.title(self.title, fontsize=16)
+        #parallels = np.arange(-90., 90., 2.)
         parallels = np.arange(-90., 90., 5.)
         meridians = np.arange(-180., 180., 5.)
 
@@ -171,8 +167,8 @@ class HMTKBaseMap(object):
         self.m.drawparallels(parallels, labels=[1, 0, 0, 0], fontsize=12)
         self.m.drawmeridians(meridians, labels=[0, 0, 0, 1], fontsize=12)
         self.m.fillcontinents(color='wheat')
-
-    def savemap(self, filename, filetype='png', papertype="a4"):
+        
+    def savemap(self, filename, filetype='png', papertype=None):
         """
         Save the figure
         """
@@ -180,6 +176,7 @@ class HMTKBaseMap(object):
                          dpi=self.dpi,
                          format=filetype,
                          papertype=papertype)
+
 
     def add_catalogue(self, catalogue, overlay=False):
         '''
@@ -234,14 +231,68 @@ class HMTKBaseMap(object):
                 leg_str = '%5.2f <= M < %5.2f' %(sym[0], sym[1])
             idx = np.logical_and(catalogue.data['magnitude'] >= sym[0],
                                  catalogue.data['magnitude'] < sym[1])
-            mag_size = 1.2 * np.min([sym[0] + 0.5, sym[1] - 0.5])
+            mag_size = 1.6 * np.min([sym[0] + 0.5, sym[1] - 0.5])
             x, y = self.m(catalogue.data['longitude'][idx],
-                          catalogue.data['latitude'][idx])
+                           catalogue.data['latitude'][idx])
             self.m.plot(x, y, sym[2], markersize=mag_size, label=leg_str)
 
-        plt.legend(bbox_to_anchor=LEGEND_OFFSET)
+        #plt.legend(bbox_to_anchor=LEGEND_OFFSET)
+        plt.legend(loc=1)
         if self.title:
-            plt.title(self.title, fontsize=16)
+            plt.title(self.title, fontsize=22)
+        if not overlay:
+            plt.show()
+
+    def add_catalogueCMT(self, catalogue, overlay=False):
+        '''
+
+        :param catalogue:
+            Earthquake catalogue as instance of
+            :class: hmtk.seismicity.catalogue.Catalogue
+
+        :param dict config:
+            Configuration parameters of the algorithm, containing the
+            following information -
+                'min_lat' Minimum value of latitude (in degrees, float)
+                'max_lat' Minimum value of longitude (in degrees, float)
+                (min_lat, min_lon) Defines the inferior corner of the map
+
+                'min_lon' Maximum value of latitude (in degrees, float)
+                'max_lon' Maximum value of longitude (in degrees, float)
+                (min_lon, max_lon) Defines the upper corner of the map
+
+        :returns:
+            Figure with the spatial distribution of the events.
+        '''
+        # Magnitudes bins and minimum marrker size
+        #min_mag = np.min(catalogue.data['magnitude'])
+        #max_mag = np.max(catalogue.data['magnitude'])
+        min_loc = np.where(np.array([symb[0] for symb in DEFAULT_SYMBOLOGY]) <
+                           np.min(catalogue.data['magnitude_cmt']))[0][-1]
+        max_loc = np.where(np.array([symb[1] for symb in DEFAULT_SYMBOLOGY]) >
+                           np.max(catalogue.data['magnitude_cmt']))[0][1]
+        symbology = DEFAULT_SYMBOLOGY[min_loc:max_loc]
+        legend_list = []
+        leg_handles = []
+        for sym in symbology:
+            # Create legend string
+            if np.isinf(sym[0]):
+                leg_str = 'M < %5.2f' % sym[1]
+            elif np.isinf(sym[1]):
+                leg_str = 'M >= %5.2f' % sym[0]
+            else:
+                leg_str = '%5.2f <= M < %5.2f' %(sym[0], sym[1])
+            idx = np.logical_and(catalogue.data['magnitude_cmt'] >= sym[0],
+                                 catalogue.data['magnitude_cmt'] < sym[1])
+            mag_size = 1.2 * np.min([sym[0] + 0.5, sym[1] - 0.5])
+            x, y = self.m(catalogue.data['longitude_cmt'][idx],
+                           catalogue.data['latitude_cmt'][idx])
+            self.m.plot(x, y, sym[2], markersize=mag_size, label=leg_str)
+
+        #plt.legend(bbox_to_anchor=LEGEND_OFFSET)
+        plt.legend(loc=1)
+        if self.title:
+            plt.title(self.title, fontsize=22)
         if not overlay:
             plt.show()
 
@@ -255,9 +306,7 @@ class HMTKBaseMap(object):
         :param float border_width:
             Line width of border (see matplotlib documentation for detail)
         """
-        lons = np.hstack([source.geometry.lons, source.geometry.lons[0]])
-        lats = np.hstack([source.geometry.lats, source.geometry.lats[0]])
-        x, y = self.m(lons, lats)
+        x, y = self.m(source.geometry.lons, source.geometry.lats)
         self.m.plot(x, y, border, linewidth=border_width)
 
     def _plot_point_source(self, source, point_marker='ks', point_size=2.0):
@@ -297,49 +346,8 @@ class HMTKBaseMap(object):
         x, y = self.m(trace_lons, trace_lats)
         self.m.plot(x, y, border, linewidth=1.3 * border_width)
 
-    def _plot_complex_fault(self, source, border='k-', border_width=1.0,
-            min_depth=0., max_depth=None, alpha=0.2):
-        """
-        Plots the simple fault source as a composite of the fault trace
-        and the surface projection of the fault.
-        :param source:
-            Fault source as instance of :class: mtkSimpleFaultSource
-        :param str border:
-            Line properties of border (see matplotlib documentation for detail)
-        :param float border_width:
-            Line width of border (see matplotlib documentation for detail)
-        """
-        if not max_depth:
-            max_depth = 70.
-        # Get outline
-        top_edge = np.column_stack([source.geometry.mesh.lons[0],
-                                    source.geometry.mesh.lats[0]])
-
-        bottom_edge = np.column_stack([source.geometry.mesh.lons[-1][::-1],
-                                       source.geometry.mesh.lats[-1][::-1]])
-        outline = np.vstack([top_edge, bottom_edge, top_edge[0,:]])
-        lons = source.geometry.mesh.lons.flatten()
-        lats = source.geometry.mesh.lats.flatten()
-        depths = source.geometry.mesh.depths.flatten()
-        norm = Normalize(vmin=min_depth, vmax=max_depth)
-        x1, y1 = self.m(lons, lats)
-        self.m.scatter(x1, y1,
-                       marker=".",
-                       s=20,
-                       c=depths,
-                       norm=norm,
-                       cmap="jet_r",
-                       alpha=alpha,
-                       linewidths=0.0,
-                       zorder=4)
-        # Plot border
-        x2, y2 = self.m(outline[:, 0], outline[:, 1])
-        self.m.plot(x2, y2, border, linewidth=border_width)
-
-
     def add_source_model(self, model, area_border='k-', border_width=1.0,
-            point_marker='ks', point_size=2.0, overlay=False, min_depth=0.,
-            max_depth=None, alpha=0.2):
+            point_marker='ks', point_size=2.0, overlay=False):
         """
         Adds a source model to the map
         :param model:
@@ -351,9 +359,6 @@ class HMTKBaseMap(object):
                 self._plot_area_source(source, area_border, border_width)
             elif isinstance(source, mtkPointSource):
                 self._plot_point_source(source, point_marker, point_size)
-            elif isinstance(source, mtkComplexFaultSource):
-                self._plot_complex_fault(source, area_border, border_width,
-                                         min_depth, max_depth, alpha)
             elif isinstance(source, mtkSimpleFaultSource):
                 self._plot_simple_fault(source, area_border, border_width)
             else:
@@ -417,9 +422,10 @@ class HMTKBaseMap(object):
                        s=(smin + data ** sscale),
                        c=colour,
                        alpha=alpha,
-                       zorder=2)
+                       zorder=4)
         if not overlay:
             plt.show()
+
 
     def _select_color_mag(self, mag):
         if (mag > 8.0):
@@ -474,104 +480,4 @@ class HMTKBaseMap(object):
                 plt.gca().add_collection(beach)
                 if not overlay:
                     plt.show()
-
-    def add_catalogue_cluster(self, catalogue, vcl, flagvector,
-            cluster_id=None, overlay=True):
-        """
-        Creates a plot of a catalogue showing where particular clusters exist
-        """
-        # Create simple magnitude scaled point basemap
-        self.add_size_scaled_points(catalogue.data['longitude'],
-                                    catalogue.data['latitude'],
-                                    catalogue.data['magnitude'],
-                                    shape="o",
-                                    alpha=0.8,
-                                    colour=(0.5, 0.5, 0.5),
-                                    smin=1.0,
-                                    sscale=1.5,
-                                    overlay=True)
-        # If cluster ID is not specified just show mainshocks
-        if cluster_id is None:
-            idx = flagvector == 0
-            self.add_size_scaled_points(catalogue.data['longitude'][idx],
-                                        catalogue.data['latitude'][idx],
-                                        catalogue.data['magnitude'][idx],
-                                        shape="o",
-                                        colour="r",
-                                        smin=1.0,
-                                        sscale=1.5,
-                                        overlay=overlay)
-            return
-        if not isinstance(cluster_id, collections.Iterable):
-            cluster_id = [cluster_id]
-        for iloc, clid in enumerate(cluster_id):
-            if iloc == (len(cluster_id) - 1):
-                # On last iteration set overlay to function overlay
-                temp_overlay = overlay
-            else:
-                temp_overlay = True
-            idx = vcl == clid
-            self.add_size_scaled_points(
-                catalogue.data["longitude"][idx],
-                catalogue.data["latitude"][idx],
-                catalogue.data["magnitude"][idx],
-                shape="o",
-                colour=DISSIMILAR_COLOURLIST[(iloc + 1) % NCOLS],
-                smin=1.0,
-                sscale=1.5,
-                overlay=temp_overlay)
-
-
-    def add_catalogueCMT(self, catalogue, overlay=False):
-        '''
-
-        :param catalogue:
-            Earthquake catalogue as instance of
-            :class: hmtk.seismicity.catalogue.Catalogue
-
-        :param dict config:
-            Configuration parameters of the algorithm, containing the
-            following information -
-                'min_lat' Minimum value of latitude (in degrees, float)
-                'max_lat' Minimum value of longitude (in degrees, float)
-                (min_lat, min_lon) Defines the inferior corner of the map
-
-                'min_lon' Maximum value of latitude (in degrees, float)
-                'max_lon' Maximum value of longitude (in degrees, float)
-                (min_lon, max_lon) Defines the upper corner of the map
-
-        :returns:
-            Figure with the spatial distribution of the events.
-        '''
-        # Magnitudes bins and minimum marrker size
-        #min_mag = np.min(catalogue.data['magnitude'])
-        #max_mag = np.max(catalogue.data['magnitude'])
-        min_loc = np.where(np.array([symb[0] for symb in DEFAULT_SYMBOLOGY]) <
-                           np.min(catalogue.data['magnitude_cmt']))[0][-1]
-        max_loc = np.where(np.array([symb[1] for symb in DEFAULT_SYMBOLOGY]) >
-                           np.max(catalogue.data['magnitude_cmt']))[0][1]
-        symbology = DEFAULT_SYMBOLOGY[min_loc:max_loc]
-        legend_list = []
-        leg_handles = []
-        for sym in symbology:
-            # Create legend string
-            if np.isinf(sym[0]):
-                leg_str = 'M < %5.2f' % sym[1]
-            elif np.isinf(sym[1]):
-                leg_str = 'M >= %5.2f' % sym[0]
-            else:
-                leg_str = '%5.2f <= M < %5.2f' %(sym[0], sym[1])
-            idx = np.logical_and(catalogue.data['magnitude_cmt'] >= sym[0],
-                                 catalogue.data['magnitude_cmt'] < sym[1])
-            mag_size = 1.2 * np.min([sym[0] + 0.5, sym[1] - 0.5])
-            x, y = self.m(catalogue.data['longitude_cmt'][idx],
-                           catalogue.data['latitude_cmt'][idx])
-            self.m.plot(x, y, sym[2], markersize=mag_size, label=leg_str)
-
-        #plt.legend(bbox_to_anchor=LEGEND_OFFSET)
-        plt.legend(loc=1)
-        if self.title:
-            plt.title(self.title, fontsize=22)
-        if not overlay:
-            plt.show()
 
